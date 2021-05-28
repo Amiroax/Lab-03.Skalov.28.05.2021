9b2a08040fdb370a64b7b140525f3b4e79f81e78
RiAxxxer2 <84098368+RiAxxxer2@users.noreply.github.com>
2021-05-29 01:28:44 +0300

    Add files via upload

---
+++ 11.19(a,b,e)/ConsoleApp10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[4] { 1, 2, 3, 5 };
+            int sum = 0;
+            int result;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            result = sum / arr.Length;
+            Console.WriteLine(sum);
+            Console.WriteLine(result);
+        }
+    }
+}
