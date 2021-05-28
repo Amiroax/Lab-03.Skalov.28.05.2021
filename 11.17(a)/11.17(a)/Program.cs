c8959607edb2737adf2d59b4a27de0261e99c674
RiAxxxer2 <84098368+RiAxxxer2@users.noreply.github.com>
2021-05-29 01:28:15 +0300

    Add files via upload

---
+++ 11.17(a)/11.17(a)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _11._17_a_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] array = { 1, 2, 3, 4, 5 }; 
+
+                for (int f = 0; f < array.Length; f++) { array[f] *= 2; Console.WriteLine(array[f]); }
+                
+            }
+        }
+    }
+}
