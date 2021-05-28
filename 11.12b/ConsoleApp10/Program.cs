9a35c3937be64002b53460930a691279305e5e81
RiAxxxer2 <84098368+RiAxxxer2@users.noreply.github.com>
2021-05-29 01:27:43 +0300

    Add files via upload

---
+++ 11.12b/ConsoleApp10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SimpleNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[30];
+            int number = 1, index = 0;
+            while (true)
+            {
+                if (IsSimple(number))
+                    arr[index++] = number;
+                if (index == 30)
+                    break;
+                number++;
+            }
+
+            foreach (int item in arr)
+                Console.Write(item + " ");
+            Console.ReadLine();
+        }
+
+        private static bool IsSimple(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
