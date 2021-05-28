ac5d4ac76c9ee7f1d4bb0d325ca7c9a2c56b8d11
RiAxxxer2 <84098368+RiAxxxer2@users.noreply.github.com>
2021-05-29 01:27:06 +0300

    Add files via upload

---
+++ 11.12a/ConsoleApp11/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(String.Join(", ", Enumerable.Range(300, 17 * 20).Where(x => x % 13 == 0 || x % 17 == 0).Take(20)));
+    }
+}
